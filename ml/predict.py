7da52a9e9fc91f43b09f918dceddc81dc3341fb0
davidchestang02-dev <david.chestang02@gmail.com>
2026-02-23 19:37:42 -0500

    Update predict.py

--- ml/predict.py
+++ ml/predict.py
@@ -1,11 +1,9 @@
 import joblib
-import pandas as pd
 from ml.feature_engineering import build_features
 
 model = joblib.load("ml/model.pkl")
 
-def predict(df):
+def predict_edges(df):
     df = build_features(df)
-    probs = model.predict_proba(df)[:, 1]
-    df["ml_edge"] = probs
+    df["ml_edge"] = model.predict_proba(df)[:, 1]
     return df

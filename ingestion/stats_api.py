a4c0f8d78fa0e6d6256a04a8017cd7eabb7d81b2
davidchestang02-dev <david.chestang02@gmail.com>
2026-02-23 19:22:03 -0500

    Create stats_api.py

---
+++ ingestion/stats_api.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def fetch_team_stats():
+    url = "https://www.sports-reference.com/cbb/seasons/2024-advanced-school-stats.html"
+    df = pd.read_html(url)[0]
+    df = df.dropna(subset=["School"])
+    return df

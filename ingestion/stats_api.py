597811fd2e7d22061cf93b3b5dccf18f1e260630
davidchestang02-dev <david.chestang02@gmail.com>
2026-02-23 21:53:32 -0500

    Update stats_api.py

--- ingestion/stats_api.py
+++ ingestion/stats_api.py
@@ -3,5 +3,7 @@ import pandas as pd
 def fetch_team_stats():
     url = "https://www.sports-reference.com/cbb/seasons/2024-advanced-school-stats.html"
     df = pd.read_html(url)[0]
+    df = df.rename(columns={"Team": "School"})
     df = df.dropna(subset=["School"])
+    
     return df
